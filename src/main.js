148bf78ff6d59de55303566bb0787a37f7e4ad86
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-02 16:38:18 +0900

    postcss

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  // ES5
+  // render function(h) {
+  //   return h(App);
+  // }
   render: h => h(App),
 }).$mount('#app')
