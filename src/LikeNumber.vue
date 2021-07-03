97de11610f839b4ccd9d26b1f3dc881a80a47910
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-03 11:39:28 +0900

    global component practice

---
+++ src/LikeNumber.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- ルートの要素は１つでないとエラーが出る -->
+<!-- divで囲って１つにしたりする -->
+<div>
+  <p>いいね（{{number}}）</p>
+  <button @click="increment">+1</button>
+</div>
+</template>
+
+<script>
+  export default { 
+    // data: function () {}
+    data() {
+      return {
+        number: 5
+      }
+    },
+    methods: {
+      increment() {
+        this.number += 1;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
